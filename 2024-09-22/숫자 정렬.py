c7e045e590db4a8f46dd5eeacf4ccd8a92e956c4
KimYongJae <golddragon939@gmail.com>
2024-09-22 02:53:44 +0900

    알고리즘 연습

---
+++ 2024-09-22/숫자 정렬.py
@@ -0,0 +1,29 @@
+# N=10
+# cnt=1
+
+# for i in range(N):
+#     print("연산횟수" + str(cnt))
+#     cnt += 1
+
+# N=10
+# cnt=1
+
+# for i in range(N):
+#     print("연산횟수"+str(cnt))
+#     cnt+=1
+
+# for i in range(N):
+#     print("연산횟수"+str(cnt))
+#     cnt+=1
+    
+# for i in range(N):
+#     print("연산횟수"+str(cnt))
+#     cnt+=1
+
+N=10
+cnt=1
+
+for i in range(N):
+    for j in range(N):
+        print("연산횟수"+str(cnt))
+        cnt+=1
